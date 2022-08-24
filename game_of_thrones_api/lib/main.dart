0d11f91d606e427407b5fc059edae217a49e89d6
schneidewind2 <rafaelvieira9@hotmail.com>
2022-08-24 00:58:03 -0300

    Descrição de todas as atividades em seus README.md

--- game_of_thrones_api/lib/main.dart
+++ game_of_thrones_api/lib/main.dart
@@ -11,11 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(      
+    return MaterialApp(
       title: 'Game of Thrones API',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: TelaPersonagens(),
